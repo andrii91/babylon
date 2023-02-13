4569fb39bc63a72a2f146f2a6adf098a29e2abe2
Andriy Lytvyn <andriy@devsalsa.com>
2023-02-13 22:58:53 +0200

    add part 2

--- dist/js/app.js
+++ dist/js/app.js
@@ -1,3 +1,36 @@
-$(window).scroll(function () {
+$( document ).ready(function() {
+  
+  $(window).scroll(function () {
     return $('.nav').toggleClass("fixed", $(window).scrollTop() > 0);
   });
+
+
+  $('.stories-slider').slick({
+    centerMode: true,
+    arrows: true,
+    slidesToShow: 1,
+    infinite: true,
+    lazyLoad: 'ondemand',
+    variableWidth: true,
+    margin: '50',
+    prevArrow: '<button class="arrow-prev"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1024 1024"><path d="M685.248 104.704a64 64 0 0 1 0 90.496L368.448 512l316.8 316.8a64 64 0 0 1-90.496 90.496L232.704 557.248a64 64 0 0 1 0-90.496l362.048-362.048a64 64 0 0 1 90.496 0z"/></svg></button>',
+    nextArrow: '<button class="arrow-next"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1024 1024"><path d="M338.752 104.704a64 64 0 0 0 0 90.496l316.8 316.8-316.8 316.8a64 64 0 0 0 90.496 90.496l362.048-362.048a64 64 0 0 0 0-90.496L429.248 104.704a64 64 0 0 0-90.496 0z"/></svg></button>',
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          centerMode: true,
+          slidesToShow: 1
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1
+        }
+      }
+    ]
+  });
+}) 
+
+
